630f15b0bb0c8f7756aa41cb2e2ca173f37b8fc8
Ku Ahmad Mudrikah <kamudrikah@gmail.com>
2014-04-14 13:07:44 +0800

    skit lg

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
+<legend>Hello</legend>
 	Perubahan
 	<p class="space"><?= "Hello World"; ?></p>
 	Lots and lots of developement is going on.
